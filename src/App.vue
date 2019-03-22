3f148d755b70004a9d0a8d93c7b38bd77eec33ec
Jackson Baker Ryan <jbr@admins-iMac.local>
2019-03-22 01:21:25 -0700

    Style mobile info card and transition

--- src/App.vue
+++ src/App.vue
@@ -1,267 +1,279 @@
 <template>
+  <div id="app" @keydown.tab="revertMenuSession" :style="{'background': backgroundColor}">
     <div
-        id="app"
-        @keydown.tab="revertMenuSession"
-        :style="{'background': backgroundColor}"
-    >
-        <div
-            v-show="$route.path.includes('publications')"
-            class="app__overlay"
-            @click="handleMenuBackgroundClickEvent"
-        ></div>
-        <transition appear>
-            <the-nav-bar
-                class="lefter"
-                :class="{'lefter__mobile--menu-open': $mq === 'sm' && mobileMenuOpen}"
+      v-show="$route.path.includes('publications')"
+      class="app__overlay"
+      @click="handleMenuBackgroundClickEvent"
+    ></div>
+    <transition appear>
+      <the-nav-bar
+        class="lefter"
+        :class="{'lefter__mobile--menu-open': $mq === 'sm' && mobileMenuOpen}"
+        :mobileMenuOpen="mobileMenuOpen"
+        @revertMenuSession="revertMenuSession"
+        @openContent="handleOpenContentEvent"
+      ></the-nav-bar>
+    </transition>
 
-                :mobileMenuOpen="mobileMenuOpen"
+    <the-site-header
+      headerTitle
+      :backgroundColor="backgroundColor"
+      :buttonSetToClose="buttonSetToClose"
+      @openMenu="handleOpenMenu"
+      @close="handleClose"
+      @logoLinkActivated="handleLogoLinkActivation"
+    ></the-site-header>
 
-                @revertMenuSession="revertMenuSession"
-                @openContent="handleOpenContentEvent"
-            ></the-nav-bar>
-        </transition>
+    <info-card
+      v-bind="infoCardData"
+      class="info-card"
+      :class="{'info-card--closed': !infoCardOpen}"
+    ></info-card>
 
-        <the-site-header
-            headerTitle=""
-            :backgroundColor="backgroundColor"
-            :buttonSetToClose="buttonSetToClose"
-            @openMenu="handleOpenMenu"
-            @close="handleClose"
-            @logoLinkActivated="handleLogoLinkActivation"
-        ></the-site-header>
+    <transition name="component-fade" mode="out-in" v-if="!$route.path.includes('publications')">
+      <router-view
+        class="content-section"
+        @click.native="revertMenuSession"
+        @focus.native="revertMenuSession"
+      ></router-view>
+    </transition>
 
-        <info-card v-show="infoCardOpen" v-bind="infoCardData" class="info-card"></info-card>
-
-        <transition
-            name="component-fade"
-            mode="out-in"
-            v-if="!$route.path.includes('publications')"
-        >
-            <router-view
-                class="content-section"
-                @click.native="revertMenuSession"
-                @focus.native="revertMenuSession"
-            >
-            </router-view>
-        </transition>
-
-        <the-site-footer class="site-footer" :style="{'background': backgroundColor, 'box-shadow': `0 50px 30px 72px ${backgroundColor}`}"></the-site-footer>
-    </div>
+    <the-site-footer
+      class="site-footer"
+      :style="{'background': backgroundColor, 'box-shadow': `0 50px 30px 72px ${backgroundColor}`}"
+    ></the-site-footer>
+  </div>
 </template>
 
 <script>
-import TheNavBar from '@/components/TheNavBar';
-import Home from '@/views/Home';
-import TheSiteFooter from './components/TheSiteFooter';
-import TheSiteHeader from './components/TheSiteHeader';
+import TheNavBar from "@/components/TheNavBar";
+import Home from "@/views/Home";
+import TheSiteFooter from "./components/TheSiteFooter";
+import TheSiteHeader from "./components/TheSiteHeader";
 
-import InfoCardData from '@/lib/dummy_data/data_infoCard.js'
-import InfoCard from '@/components/InfoCard'
+import InfoCardData from "@/lib/dummy_data/data_infoCard.js";
+import InfoCard from "@/components/InfoCard";
 
-import styles from '@/lib/styles.js';
+import styles from "@/lib/styles.js";
 
 export default {
-    components: {
-        TheSiteHeader,
-        TheSiteFooter,
-        TheNavBar,
-        Home,
-        InfoCard
+  components: {
+    TheSiteHeader,
+    TheSiteFooter,
+    TheNavBar,
+    Home,
+    InfoCard
+  },
+  data: function() {
+    return {
+      mobileMenuOpen: false,
+      infoCardOpen: true,
+      styles: styles,
+      infoCardData: InfoCardData
+    };
+  },
+  computed: {
+    buttonSetToClose: function() {
+      return this.mobileMenuOpen || this.infoCardOpen;
     },
-    data: function() {
-        return {
-            mobileMenuOpen: false,
-            infoCardOpen: true,
-            styles: styles,
-            infoCardData: InfoCardData
-        }
-    },
-    computed: {
-        buttonSetToClose: function() {
-            return this.mobileMenuOpen || this.infoCardOpen;
-        },
-        backgroundColor: function() {
-            return this.infoCardOpen && this.$mq === 'sm' ? styles.backgroundColors.card : styles.backgroundColors.default
-        }
-    },
-    methods: {
-        /** */
-        handleOpenMenu: function() {
-            this.mobileMenuOpen = true;
-        },
-
-        /** */
-        handleClose: function() {
-            if (this.mobileMenuOpen) this.mobileMenuOpen = false
-            if (this.infoCardOpen) this.infoCardOpen = false
-        },
-
-        /** */
-        handleMenuBackgroundClickEvent: function() {
-            this.revertMenuSession();
-        },
-
-        /**
-         * Process to handle logo click event
-         */
-        handleLogoLinkActivation: function() {
-            this.$router.push('/');
-            let visitCount = this.$store.getters['routerNav/getVisitCount'];
-            if (visitCount > 0) {
-                this.$store.dispatch('routerNav/resetVisitCount');
-                this.$store.dispatch('menuTree/closeMenuExpansions');
-            }
-        },
-
-        /**
-         * Handles the event from the user requesting an article
-         * @param {string} routerLinkLocation
-         * @param {boolean} keyboardEvent
-         */
-        handleOpenContentEvent: function(routerLinkLocation, keyBoardEvent) {
-            this.mobileMenuOpen = false;
-            this.$store.dispatch('routerNav/resetVisitCount');
-            if (keyBoardEvent) {
-                this.$router.push(routerLinkLocation);
-            }
-            this.$store.dispatch('menuTree/closeMenuExpansions');
-        },
-
-        /**
-         * Returns the app to the state that was before the menu session
-         */
-        revertMenuSession: function() {
-            let visitCount = this.$store.getters['routerNav/getVisitCount'];
-            // If menu session didn't include clicked publication links
-            if (visitCount > 0) {
-                this.$router.go(visitCount * -1);
-                this.$store.dispatch('routerNav/resetVisitCount');
-            } else if (this.$route.path.includes('publications')) { // if menu session began open b/c first visit to site was to collection
-                this.$router.push('/');
-                this.$store.dispatch('routerNav/resetVisitCount');
-            }
-            this.$store.dispatch('menuTree/closeMenuExpansions');
-        }
+    backgroundColor: function() {
+      return this.infoCardOpen && this.$mq === "sm"
+        ? styles.backgroundColors.card
+        : styles.backgroundColors.default;
     }
-}
+  },
+  methods: {
+    /** */
+    handleOpenMenu: function() {
+      this.mobileMenuOpen = true;
+    },
+
+    /** */
+    handleClose: function() {
+      if (this.mobileMenuOpen) this.mobileMenuOpen = false;
+      if (this.infoCardOpen) this.infoCardOpen = false;
+    },
+
+    /** */
+    handleMenuBackgroundClickEvent: function() {
+      this.revertMenuSession();
+    },
+
+    /**
+     * Process to handle logo click event
+     */
+    handleLogoLinkActivation: function() {
+      this.$router.push("/");
+      let visitCount = this.$store.getters["routerNav/getVisitCount"];
+      if (visitCount > 0) {
+        this.$store.dispatch("routerNav/resetVisitCount");
+        this.$store.dispatch("menuTree/closeMenuExpansions");
+      }
+    },
+
+    /**
+     * Handles the event from the user requesting an article
+     * @param {string} routerLinkLocation
+     * @param {boolean} keyboardEvent
+     */
+    handleOpenContentEvent: function(routerLinkLocation, keyBoardEvent) {
+      this.mobileMenuOpen = false;
+      this.$store.dispatch("routerNav/resetVisitCount");
+      if (keyBoardEvent) {
+        this.$router.push(routerLinkLocation);
+      }
+      this.$store.dispatch("menuTree/closeMenuExpansions");
+    },
+
+    /**
+     * Returns the app to the state that was before the menu session
+     */
+    revertMenuSession: function() {
+      let visitCount = this.$store.getters["routerNav/getVisitCount"];
+      // If menu session didn't include clicked publication links
+      if (visitCount > 0) {
+        this.$router.go(visitCount * -1);
+        this.$store.dispatch("routerNav/resetVisitCount");
+      } else if (this.$route.path.includes("publications")) {
+        // if menu session began open b/c first visit to site was to collection
+        this.$router.push("/");
+        this.$store.dispatch("routerNav/resetVisitCount");
+      }
+      this.$store.dispatch("menuTree/closeMenuExpansions");
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-    @import 'styles/_settings';
-    @import 'styles/focusable';
+@import "styles/_settings";
+@import "styles/focusable";
 
-    * {
-        margin: 0;
-        padding: 0;
-        color: black;
-        list-style-type: none;
-        font-weight: normal;
-        margin-block-start: 0;
-        margin-block-end: 0;
-        font-size: 12px;
-        text-decoration: none;
-        @media screen and (max-width: $breakSmall) {
-            font-size: 0.9em;
-        }
-    }
+* {
+  margin: 0;
+  padding: 0;
+  color: black;
+  list-style-type: none;
+  font-weight: normal;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  font-size: 12px;
+  text-decoration: none;
+  @media screen and (max-width: $breakSmall) {
+    font-size: 0.9em;
+  }
+}
 
-    body {
-        background: $bgColor;
-    }
+body {
+  background: $bgColor;
+}
 
-    #app {
-        position: absolute;
-        width: $appWidth; // 100vw adds a scroll bar to the bottom of page
-        height: 100vh;
+#app {
+  position: absolute;
+  width: $appWidth; // 100vw adds a scroll bar to the bottom of page
+  height: 100vh;
 
-        color: #2c3e50;
+  color: #2c3e50;
 
-        background: transparent;
+  background: transparent;
 
-        text-align: center;
-        font-family: $sansSerifFont;
-        -moz-osx-font-smoothing: grayscale;
-        -webkit-font-smoothing: antialiased;
+  text-align: center;
+  font-family: $sansSerifFont;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
 
-        @media screen and (max-width: $breakSmall) {
-            width: 100vw;
-        }
-    }
+  @media screen and (max-width: $breakSmall) {
+    width: 100vw;
+  }
+}
 
-    .app__overlay {
-        width: 100%;
-        height: 100%;
-    }
+.app__overlay {
+  width: 100%;
+  height: 100%;
+}
 
-    .lefter {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: $lefterWidth;
-        z-index: 10;
-        margin-top: 100px;
+.lefter {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: $lefterWidth;
+  z-index: 10;
+  margin-top: 100px;
 
-        @media screen and (max-width: $breakSmall) {
-            transform: translateX(-100vw);
-            transition: 300ms ease-in;
-            height: 100%;
-            margin-top: 60px;
-            width: 100vw;
-        }
-    }
+  @media screen and (max-width: $breakSmall) {
+    transform: translateX(-100vw);
+    transition: 300ms ease-in;
+    height: 100%;
+    margin-top: 60px;
+    width: 100vw;
+  }
+}
 
-    .lefter__mobile--menu-open {
-        transform: translateX(0vw);
-        // left: 0;
-        transition: 300ms ease-out;
-    }
+.lefter__mobile--menu-open {
+  transform: translateX(0vw);
+  // left: 0;
+  transition: 300ms ease-out;
+}
 
-    .info-card {
-        background: rgb(247, 247, 233);
-        @media screen and (max-width: $breakSmall) {
-            position: absolute;
-            top: 80px;
-            padding: 12px;
-            margin-bottom: 50px;
-        }
-    }
+.info-card {
+  top: 80px;
+  background: rgb(247, 247, 233);
+  transition: 200ms ease-in;
+  @media screen and (max-width: $breakSmall) {
+    position: absolute;
+    left: 0;
+    padding: 12px;
+    margin-bottom: 50px;
+  }
+}
 
-    .content-section {
-        top: 0;
-        left: 0;
-        width: calc(#{$appWidth} - #{$lefterWidth} * 1.5);
-        padding: 120px 0 0 calc(#{$lefterWidth} * 1.5);
+.info-card--closed {
+  transform: translateX(100vw);
+  transition: 200ms ease-in;
 
-        overflow-x: hidden;
+  @media screen and (max-width: $breakSmall) {
+    // transform: translateX(0vw);
+  }
+}
 
-        @media screen and (max-width: $breakSmall) {
-            margin: 72px 10px 10px 10px;
-            width: calc(100vw - 20px);
-            padding: 0;
-        }
-        @media screen and (min-width: $breakSmall) and (max-width: $breakMedium) {
-            margin: 100px 10px 10px 10px;
-            padding: 0px 0 0 calc(#{$lefterWidth} * 1.5);
-        }
-    }
+.content-section {
+  top: 0;
+  left: 0;
+  width: calc(#{$appWidth} - #{$lefterWidth} * 1.5);
+  padding: 120px 0 0 calc(#{$lefterWidth} * 1.5);
 
-    .site-footer {
-        position: fixed;
-        bottom: 0;
-        width: 100%;
-        height: $footerHeight;
-        background: #faf7f7;
-        box-shadow: 0 50px 30px 72px #faf7f7;
+  overflow-x: hidden;
 
-        @media screen and (max-width: $breakSmall) {
-            font-size: 1.5em;
-        }
-    }
+  @media screen and (max-width: $breakSmall) {
+    margin: 72px 10px 10px 10px;
+    width: calc(100vw - 20px);
+    padding: 0;
+  }
+  @media screen and (min-width: $breakSmall) and (max-width: $breakMedium) {
+    margin: 100px 10px 10px 10px;
+    padding: 0px 0 0 calc(#{$lefterWidth} * 1.5);
+  }
+}
 
-    .component-fade-enter-active, .component-fade-leave-active {
-        transition: opacity .3s ease;
-    }
-    .component-fade-enter, .component-fade-leave-to {
-        opacity: 0;
-    }
+.site-footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: $footerHeight;
+  background: #faf7f7;
+  box-shadow: 0 50px 30px 72px #faf7f7;
+
+  @media screen and (max-width: $breakSmall) {
+    font-size: 1.5em;
+  }
+}
+
+.component-fade-enter-active,
+.component-fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.component-fade-enter,
+.component-fade-leave-to {
+  opacity: 0;
+}
 </style>
--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -1,218 +1,241 @@
 <template>
-    <div class="info-card">
-        <div class="info-card__floating-paper">
-            <div class="info-card__header">
-                <p class="info__call-number">
-                    G<br>{{year}}
-                </p>
-                <address class="info__authors">
-                    <span
-                        v-for="(author, index) in authorList"
-                        :key="`author-${index}`"
-                        class="info__author"
-                    >
-                        {{author}}<br>
-                    </span>
-                </address>
-            </div>
-            <div class="info-card__title">
-                <h1>
-                    {{title}}
-                    <span v-if="subtitle" class="info-card__subtitle">
-                        &nbsp;({{subtitle}})
-                    </span>
-                </h1>
-            </div>
-            <div class="info-card__abstract">
-                <h2 class="info-card__abstract-title">
-                    ABSTRACT
-                </h2>
-                <p class="info-card__abstract-body">
-                    {{abstract}}
-                </p>
-            </div>
-            <ol class="info-card__tags">
-                <li
-                    v-for="(keyword, index) in keywordList"
-                    :key="`tag-${index}`"
-                    class="info-card__tag"
-                >
-                    {{index}}.&nbsp;&nbsp;&nbsp;{{keyword}}
-                </li>
-            </ol>
-            <div class="info-card__footer">
-                <p class="info-card__date-code">
-                    G&nbsp;&nbsp;{{year}}&nbsp;&nbsp;&nbsp;{{String(dateFormatted.getDate()).padStart(2, 0)}}&ndash;{{String(dateFormatted.getMonth()).padStart(2, 0)}}
-                </p>
-                <p class="info-card__publication">
-                    <a :href="publication.pageUrl">
-                        {{publication.name}}
-                    </a>
-                </p>
-                <p class="info-card__copyright">
-                    &copy;&nbsp;{{authorsShortFormat}}
-                </p>
-            </div>
-        </div>
+  <div class="info-card">
+    <div class="info-card__floating-paper">
+      <div class="info-card__header">
+        <p class="info__call-number">
+          G
+          <br>
+          {{year}}
+        </p>
+        <address class="info__authors">
+          <span v-for="(author, index) in authorList" :key="`author-${index}`" class="info__author">
+            {{author}}
+            <br>
+          </span>
+        </address>
+      </div>
+      <div class="info-card__title">
+        <h1>
+          {{title}}
+          <span v-if="subtitle" class="info-card__subtitle">&nbsp;({{subtitle}})</span>
+        </h1>
+      </div>
+      <div class="info-card__abstract">
+        <h2 class="info-card__abstract-title">abstract</h2>
+        <p class="info-card__abstract-body">{{abstract}}</p>
+      </div>
+      <ol class="info-card__tags">
+        <li v-for="(keyword, index) in keywordList" :key="`tag-${index}`" class="info-card__tag">
+          <a href="#tag">{{index + 1}}.&nbsp;&nbsp;&nbsp;{{keyword}}</a>
+        </li>
+      </ol>
+      <div class="info-card__footer">
+        <p
+          class="info-card__date-code"
+        >G&nbsp;&nbsp;{{year}}&nbsp;&nbsp;&nbsp;{{String(dateFormatted.getDate()).padStart(2, 0)}}&ndash;{{String(dateFormatted.getMonth()).padStart(2, 0)}}</p>
+        <p class="info-card__publication">
+          <a :href="publication.pageUrl">{{publication.name}}</a>
+        </p>
+        <p class="info-card__copyright">&copy;&nbsp;{{authorsShortFormat}}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import InfoCardData from '@/lib/dummy_data/data_infoCard.js'
+import InfoCardData from "@/lib/dummy_data/data_infoCard.js";
 
 export default {
-    props: {
-        id: String,
-        authors: {
-            type:String,
-            default: ""
-        },
-        title: String,
-        subtitle: {
-            type: String,
-            default: ""
-        },
-        date: String,
-        abstract: String,
-        keywords: {
-            type: Array,
-            default: function() {
-                return []
-            }
-        },
-        publication: {
-            type: Object,
-            default: function() {
-                return {
-                    name: "",
-                    pageUrl: ""
-                }
-            }
-        }
+  props: {
+    id: String,
+    authors: {
+      type: String,
+      default: ""
     },
-    data: function() {
+    title: String,
+    subtitle: {
+      type: String,
+      default: ""
+    },
+    date: String,
+    abstract: String,
+    keywords: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    },
+    publication: {
+      type: Object,
+      default: function() {
         return {
-            dateFormatted: new Date(InfoCardData.date)
-        }
-    },
-    methods: {
-        
-    },
-    computed: {
-        year: function() {
-            return this.dateFormatted.getFullYear()
-        },
-        authorList: function() {
-            return this.authors.split(";")
-        },
-        authorsShortFormat: function() {
-            let formattedFirstAuthor = this.authorList[0].split(', ').reverse().join(' ')
-            if (this.authorList.length > 1) {
-                return formattedFirstAuthor + ', et al.'
-            } else {
-                return formattedFirstAuthor
-            }
-        },
-        keywordList: function() {
-            return this.keywords.map(tag => tag.replace(/ /g, "--"))
-        }
+          name: "",
+          pageUrl: ""
+        };
+      }
     }
-}
+  },
+  data: function() {
+    return {
+      dateFormatted: new Date(InfoCardData.date)
+    };
+  },
+  methods: {},
+  computed: {
+    year: function() {
+      return this.dateFormatted.getFullYear();
+    },
+    authorList: function() {
+      return this.authors.split(";");
+    },
+    authorsShortFormat: function() {
+      let formattedFirstAuthor = this.authorList[0]
+        .split(", ")
+        .reverse()
+        .join(" ");
+      if (this.authorList.length > 1) {
+        return formattedFirstAuthor + ", et al.";
+      } else {
+        return formattedFirstAuthor;
+      }
+    },
+    keywordList: function() {
+      return this.keywords.map(tag => tag.replace(/ /g, "--"));
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-    @import '../styles/_settings';
-    @import '../styles/focusable';
-    
-    .info-card {
-        font-family: 'PT Serif Caption';
+@import "../styles/_settings";
+@import "../styles/focusable";
 
-        @media screen and (max-width: $breakSmall) {
-            font-size: 2.5em;
-            line-height: 0.9em;
-        }
-    }
+.info-card {
+  font-family: "PT Serif Caption";
+  transition: 200ms ease-in;
 
-    .info-card > * {
-        margin-bottom: 15px;
-    }
+  @media screen and (max-width: $breakSmall) {
+    font-size: 2.5em;
+    line-height: 0.9em;
+  }
+}
 
-    .info-card__floating-paper {
-        display: flex;
-        flex-direction: column;
-    }
+.info-card > * {
+  margin-bottom: 15px;
+}
 
-    .info-card__header {
-        display: flex;
-        justify-content: space-between;
-        order: 1;
+.info-card a {
+  //   text-decoration: underline;
+  border-bottom: 2px solid black;
+}
 
-        font-size: 1em;
-        line-height: 0.9em;
-    }
+.info-card__floating-paper {
+  display: flex;
+  flex-direction: column;
+}
 
-    .info__call-number {
-        text-align: left;
-    }
+.info-card__header {
+  display: flex;
+  justify-content: space-between;
+  order: 1;
 
-    .info__authors {
-        text-align: right;
-        font-family: 'PT Serif';
-    }
+  font-size: 1em;
+  line-height: 0.9em;
+}
 
-    .info-card__title {
-        order: 2;
-        font-family: 'PT Serif';
-        font-weight: bold;
-        text-align: left;
-    }
+.info__call-number {
+  text-align: left;
+}
 
-    .info-card__subtitle {
-        font-family: 'PT Serif Caption';
-        font-weight: normal;
-        font-style: italic;
-    }
+.info__authors {
+  text-align: right;
+  font-family: "PT Serif";
+}
 
-    .info-card__abstract {
-        text-align: left;
-        font-size: 0.75em;
+.info-card__title {
+  order: 2;
+  font-family: "PT Serif";
+  font-weight: bold;
+  text-align: left;
+  margin: 1.2em 0 0 0;
+  font-size: 1em;
+}
 
-        @media screen and (max-width: $breakSmall) {
-            order: 5;
-        }
-    }
+.info-card__subtitle {
+  font-family: "PT Serif Caption";
+  font-weight: normal;
+  font-style: italic;
+}
 
-    .info-card__tags {
-        order: 4;
-        text-align: left;
+.info-card__abstract {
+  text-align: left;
+  font-size: 0.75em;
+  margin: 2em 0 0 0;
 
-        @media screen and (max-width: $breakSmall) {
-            order: 3;
-        }
-    }
+  @media screen and (max-width: $breakSmall) {
+    order: 6;
+  }
+}
 
-    .info-card__footer {
-        order: 5;
-        @media screen and (max-width: $breakSmall) {
-            order: 4;
-        }
-    }
+.info-card__abstract-title {
+  margin: 0 0 1.5em 0;
+  text-transform: uppercase;
+}
 
-    .info-card__date-code {
-        text-align: left;
-        font-family: 'PT Serif';
-        font-weight: bold;
-    }
+.info-card__abstract-body {
+  text-indent: 1.5em;
+}
 
-    .info-card__publication {
-        text-align: right;
-        font-family: 'PT Serif';
-        font-weight: bold;
-        font-size: 1.1em;
-    }
+.info-card__tags {
+  order: 4;
+  text-align: left;
+  font-size: 0.7em;
+  line-height: 2.5em;
+  margin: 1em 0 0 0;
+  text-transform: capitalize;
 
-    .info-card__copyright {
-        text-align: left;
-        font-size: 0.6em;
-    }
+  @media screen and (max-width: $breakSmall) {
+    order: 3;
+  }
+}
+
+.info-card__footer {
+  order: 5;
+  margin: 1.5em 0 0 0;
+  @media screen and (max-width: $breakSmall) {
+    order: 4;
+  }
+}
+
+.info-card__date-code {
+  text-align: left;
+  font-family: "PT Serif";
+  font-weight: bold;
+}
+
+.info-card__publication {
+  text-align: right;
+  font-family: "PT Serif";
+  font-weight: bold;
+  font-size: 1.1em;
+}
+
+.info-card__copyright {
+  text-align: left;
+  font-size: 0.6em;
+}
+
+.info-card__divider {
+  z-index: 0;
+  background: black;
+  height: 100px;
+  position: relative;
+  top: 0;
+  @media screen and (max-width: $breakSmall) {
+    width: 100vw;
+    order: 5;
+    // border: 3px solid black;
+    box-shadow: 0px 5px 17px 2px black;
+  }
+}
 </style>
